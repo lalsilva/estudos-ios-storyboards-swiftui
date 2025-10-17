223a8ae06c6ff72a00117abd45c33b2fed6ef580
Luiz Silva <lasilva35@midgard.local>
2025-10-17 19:05:18 -0300

    feat: add RGB color picker with interactive sliders and dynamic icon

--- swiftui/final/SwiftUI/SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ swiftui/final/SwiftUI/SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var hello = "Hello World!"
+    @State var red = 1.0
+    @State var green = 0.0
+    @State var blue = 0.0
+    
     var body: some View {
         VStack {
-            TextField("Type your name", text: $hello)
-            Text(hello)
+            Text("Color Picker")
                 .font(.title)
+                .fontWeight(.light)
+            Image(systemName: "dog.circle.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(Color(red: red, green: green, blue: blue))
+            HStack {
+                Text("Red:")
+                Slider(value: $red)
+            }
+            HStack {
+                Text("Green:")
+                Slider(value: $green)
+            }
+            HStack {
+                Text("Blue:")
+                Slider(value: $blue)
+            }
         }
         .padding()
     }
