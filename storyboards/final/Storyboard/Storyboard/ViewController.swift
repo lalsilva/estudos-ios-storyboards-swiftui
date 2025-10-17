74a6d91501693a196c38d80f4a5220f1b9ca2337
Luiz Silva <lasilva35@midgard.local>
2025-10-17 09:23:14 -0300

    feat: Melhora UX do app de saudação (Challenge Módulo 2)
    - Inicializa label com texto vazio no viewDidLoad()
    - Remove texto placeholder do Interface Builder
    - Melhora experiência do usuário com estado inicial limpo
    
    Challenge do Módulo 2 concluído

--- storyboards/final/Storyboard/Storyboard/ViewController.swift
+++ storyboards/final/Storyboard/Storyboard/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textField.becomeFirstResponder()
+        label.text = ""
     }
     
     @IBAction func buttonWasPressed(_ sender: UIButton) {
