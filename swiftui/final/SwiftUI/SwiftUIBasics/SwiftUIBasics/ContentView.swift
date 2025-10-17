0a09d1a55c61e98b9c3b430f670873a4da8bd8d3
Luiz Silva <lasilva35@midgard.local>
2025-10-17 18:36:42 -0300

    feat: implement TextField with @State binding for text input

--- swiftui/final/SwiftUI/SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ swiftui/final/SwiftUI/SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -8,54 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var hello = "Hello World!"
     var body: some View {
         VStack {
-            Text("Weather")
-                .font(.largeTitle)
-                .fontWeight(.thin)
-                .foregroundStyle(.gray)
-            HStack {
-                Image(systemName: "sun.max.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.yellow)
-                Text("Sunday")
-            }
-            HStack {
-                Image(systemName: "sun.dust.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.blue)
-                Text("Monday")
-            }
-            HStack {
-                Image(systemName: "cloud.drizzle.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.gray)
-                Text("Tuesday")
-            }
-            HStack {
-                Image(systemName: "cloud.rain.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.gray)
-                Text("Wednesday")
-            }
-            HStack {
-                Image(systemName: "cloud.rain.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.gray)
-                Text("Thursday")
-            }
-            HStack {
-                Image(systemName: "sun.dust.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.blue)
-                Text("Friday")
-            }
-            HStack {
-                Image(systemName: "sun.rain.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.blue)
-                Text("Saturday")
-            }
+            TextField("Type your name", text: $hello)
+            Text(hello)
+                .font(.title)
         }
         .padding()
     }
